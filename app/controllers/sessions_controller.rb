3fc348734ee2b97f8c12be0c313b564b562a25ee
Mary Burford <mburford@paperlesspost.net>
2015-07-19 18:07:11 -0400

    add create method on accout model, sesssion controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create  
-    raise request.env["omniauth.auth"].to_yaml  
+  	auth = request.env["omniauth.auth"]  
+    account = Account.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Account.create_with_omniauth(auth)  
   end  
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,11 @@
 class Account < ActiveRecord::Base
+	def self.create_with_omniauth(auth)  
+    create! do |account|  
+      account.provider = auth["provider"]  
+      account.provider_id = auth["uid"]  
+      account.name = auth["user_info"]["name"]
+      account.token = auth["oauth_token"]
+      account.secret = auth["oauth_token_secret"]
+    end
+  end
 end
