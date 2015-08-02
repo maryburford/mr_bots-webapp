393d525d9f68b0a5ecfdfe720178897f2a5d757a
Mary Burford <mburford@paperlesspost.net>
2015-08-02 15:41:40 -0400

    swap db to postgres, to run locally you will need to cange database.yml config

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150727122548) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
