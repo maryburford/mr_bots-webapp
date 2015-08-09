5946556e3f47a7aa73e134d6918cc901ad31cfbf
Patrick Steadman <ptsteadman@gmail.com>
2015-08-09 16:58:53 -0400

    modified gitignore and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727122548) do
+ActiveRecord::Schema.define(version: 20150803005243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 20150727122548) do
 
   create_table "engagements", force: :cascade do |t|
     t.integer  "campaign_id"
-    t.integer  "prey_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "prey_id",     limit: 8
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
 end
