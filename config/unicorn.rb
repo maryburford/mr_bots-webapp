7a80e901300064f961ce370b46330e9831665afc
Ubuntu <ubuntu@ip-172-31-39-234.us-west-2.compute.internal>
2015-08-12 19:57:25 +0000

    database config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ working_directory File.expand_path("../..", __FILE__)
 worker_processes 5
 listen "/tmp/unicorn.sock"
 timeout 30
-pid "/tmp/unicorn_mrbot.pid"
+pid "/tmp/unicorn_mr_bots-webapp.pid"
 stdout_path "/home/ubuntu/mr_bots-webapp/log/unicorn.log"
 stderr_path "/home/ubuntu/mr_bots-webapp/log/unicorn.log"
