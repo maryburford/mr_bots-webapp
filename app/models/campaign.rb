09589cef743ce96d265bd8f295c9cdc3d8eb9c24
Patrick Steadman <ptsteadman@gmail.com>
2015-12-23 17:46:26 -0500

    Maintain bot type if validation fails

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,7 +3,7 @@ class Campaign < ActiveRecord::Base
   validates :account_id, presence: true
   validates :engagements_per_day, numericality: { only_integer: true, greater_than: 0}
   validates :engagements_per_prey, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 10}
-  validates :engagement_type, inclusion: { in: %w(Favorite) }
+  validates :engagement_type, inclusion: { in: %w(Favorite Clone) }
   validate :target_valid
 
   def target_valid
