5597fb111acfb519fe0e4f5da94c1bf4955625d5
Mary Burford <mburford@paperlesspost.net>
2015-12-28 16:44:51 -0500

    Support double clone mode

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,12 +7,21 @@ class Campaign < ActiveRecord::Base
   validate :target_valid
 
   def target_valid
-    if !Account.user_exists?(target) then 
-      errors.add(:target, "account is invalid")
+    if target =~ /,/
+      targets = target.split(',')
+      for target in targets do
+        if !Account.user_exists?(target) then
+          errors.add(:target, "account is invalid")
+        end
+      end
+    else
+    if !Account.user_exists?(target) then
+        errors.add(:target, "account is invalid")
+      end
     end
   end
 
-  def less_than_max_engagements 
+  def less_than_max_engagements
     if engagements_per_day > Account.find(account_id).get_max_engagements_per_day() then
       errors.add(:engagements_per_day, "greater than the maximum allowed value.")
     end
